aaef1027503eefd7d0fea643782d002a41453465
David Valin <hola@davidvalin.com>
2025-03-29 04:42:40 +0100

    new diagram + start using voice

--- ttalk.sh
+++ ttalk.sh
@@ -5,23 +5,48 @@
 #
 # https://www.github.com/DavidValin/ai-ttalk
 
-wait_and_record() {
+wait_to_record() {
   while true; do
-    read -rsp $'\n# \n' -n1
-    execute_command
+    PART=$(get_prompt_from_speech | tr '[:upper:]' '[:lower:]' | tr -d ' \n\t.,')
+    # read -rsp $'\n# \n' -n1
+    echo "'$PART'"
+    if [ "$PART" = "heylisten" ]; then
+      echo "ok"
+      kill -9 $SPINNER_PID 2>/dev/null
+      callback
+      spinner &
+      SPINNER_PID=$!
+      trap "kill $SPINNER_PID" EXIT
+    fi
   done
 }
 
-execute_command() {
-  echo "..."
-  HOME_DIR=$(echo $HOME)
-  PROMPT_TEXT=$(rec -q -V1 -r 16000 -b 16 -t wav - silence 1 0.50 0.1% 1 00:00:02 1% | whisper-cli -m $HOME_DIR/.whisper-models/ggml-large-v3-q5_0.bin -np -nt -f - | tr -d '\n')
-  echo $PROMPT_TEXT
-  ask $PROMPT_TEXT
+spinner() {
+  while true; do
+    for i in '⠋' '⠙' '⠹' '⠽' '⠾' '⠿'; do
+      printf "\r$i "
+      sleep 0.2
+    done
+  done
 }
 
-# Iniciar el script
-echo "\nLet's talk. Press any hey to record."
+get_prompt_from_speech() {
+  HOME_DIR=$(echo $HOME)
+  PROMPT_TEXT=$(rec -q -V1 -r 16000 -b 16 -t wav - silence 1 2 0.1% 1 00:00:02 1% | whisper-cli -m $HOME_DIR/.whisper-models/ggml-large-v3-q5_0.bin -np -nt -f - | tr -d '\n')
+  echo $PROMPT_TEXT
+}
+
+callback() {
+  PROMPT_TEXT="$(get_prompt_from_speech)"
+  printf "%s" "$PROMPT_TEXT"
+  ask "$PROMPT_TEXT"
+}
+
+echo "\nLet's talk. Say 'hey, man' to start recording"
 echo "It will stop on 2 second silence:"
 echo "---------------------------------------"
-wait_and_record
+
+spinner &
+SPINNER_PID=$!
+trap "kill $SPINNER_PID" EXIT
+wait_to_record
